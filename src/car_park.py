3615e90eddcfbc3aa575f8d7000ef15b5c333b5b
patricklawsontafe <20101549@tafe.wa.edu.au>
2023-12-08 12:02:27 +0800

    Updates to files

--- src/car_park.py
+++ src/car_park.py
@@ -1,21 +1,28 @@
 from sensor import Sensor
 from display import Display
 
+from pathlib import Path
+from datetime import datetime
+
+
 class CarPark:
-    def __init__(self, location="Unknown", capacity=0, plates=None, sensors=None, displays=None):
+    def __init__(self, location="Unknown", capacity=0, plates=None, sensors=None, displays=None,
+                 log_file=Path("log.txt")):
         self.location = location
         self.sensors = sensors or []
         self.plates = plates or []
         self.capacity = capacity
         self.displays = displays or []
+        self.log_file = log_file if isinstance(log_file, Path) else Path(log_file)
+        self.log_file.touch(exist_ok=True)
 
     def __str__(self):
         return f"Car park '{self.location}' contains {self.capacity} bays."
-    
+
     def register(self, component):
         if not isinstance(component, (Sensor, Display)):
             raise TypeError("Object must be a Sensor or Display")
-        
+
         if isinstance(component, Sensor):
             self.sensors.append(component)
         elif isinstance(component, Display):
@@ -24,17 +31,22 @@ class CarPark:
     def add_car(self, plate):
         self.plates.append(plate)
         self.update_displays()
+        self._log_car_activity(plate, "entered")
 
     def remove_car(self, plate):
         self.plates.remove(plate)
         self.update_displays()
+        self._log_car_activity(plate, "exited")
 
     def update_displays(self):
         data = {"available_bays": self.available_bays, "temperature": 25}
         for display in self.displays:
             display.update(data)
 
-    
+    def _log_car_activity(self, plate, action):
+        with self.log_file.open("a") as f:
+            f.write(f"{plate} {action} at {datetime.now():%Y-%m-%d %H:%M:%S}\n")
+
     @property
     def available_bays(self):
         if len(self.plates) >= self.capacity:
